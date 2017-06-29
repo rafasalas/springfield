e09723e7c332ee9e76e30f71e59e304e57fcfdd6
salas <rafa.salas@gmail.com>
2017-06-29 14:05:50 +0200

    receiving...

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,16 +2,118 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofBackground(0, 0, 0);
 
+
+
+
+
+
+	ofNoFill();
+
+	ofEnableSmoothing();
+	ofEnableAlphaBlending();
+
+	//musica.loadSound("musica.mp3");
+
+	//musica.play();
+	//play = true;
+	receiver.setup(6449);
+
+	for (int i = 0; i<512; i++) {
+		coorX[i] = ofRandom(0, 1000);
+		iniY[i] = ofRandom(100, 600);
+
+
+
+
+	}
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 
+
+
+
+
+
+
+
+	while (receiver.hasWaitingMessages())
+	{
+		ofxOscMessage m;
+		receiver.getNextMessage(&m);
+
+		msg = std::string("/fft_value");
+		addr = std::string(m.getAddress());
+		//if (addr.compare(msg) == 0) {
+		if (msg == addr) {
+			for (int j = 0; j < 512; j++) {
+
+
+				coorY[j] = iniY[j] - m.getArgAsFloat(j) * 30;
+				if (coorY[j] < 20) {
+					coorY[j] = 20;
+				}
+
+			}
+		}
+
+
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	ofSetVerticalSync(true);
+	ofSetFrameRate(60);
+	for (int j = 0; j<512; j++) {
+		ofSetColor(255, 0, 0);
+
+		//ofLine (coorX[j],coorY[j],coorX[j+1],coorY[j+1]);
+		ofNoFill();
+
+
+		rama(coorX[j], coorY[j], 1100 - coorY[j], j + 25);
+		ofFill();
+		ofSetColor(255, 255, 255, 20);
+
+		ofCircle(coorX[j], coorY[j], 30);
+		ofSetColor(255, 255, 255, 125);
+
+		ofCircle(coorX[j], coorY[j], 10);
+		ofSetColor(255, 0, 0, 225);
+
+		ofCircle(coorX[j], coorY[j], 5);
+
+
+
+
+
+	}
+}
+void ofApp::rama(int inicioX, int inicioY, int longitud, int ancho) {
+	int medioX = inicioX;
+	int medioY = inicioY + longitud / 2;
+	int finalX = inicioX;
+	int finalY = inicioY + longitud;
+	int control1X = inicioX - ancho;
+	int control1Y = inicioY + longitud / 4;
+	int control2X = inicioX + ancho;
+	int control2Y = inicioY + 3 * (longitud / 4);
+
+	ofBeginShape();
+
+	ofVertex(inicioX, inicioY);
+	ofBezierVertex(inicioX, inicioY, control1X, control1Y, medioX, medioY);
+	ofBezierVertex(medioX, medioY, control2X, control2Y, finalX, finalY);
+	ofVertex(finalX, finalY);
+	ofEndShape();
+
+
+
+
 
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,13 +1,51 @@
 #pragma once
 
 #include "ofMain.h"
-
+#include "ofxOsc.h"
 class ofApp : public ofBaseApp{
 
 	public:
 		void setup();
 		void update();
 		void draw();
+
+
+		
+		void rama(int inicioX, int inicioY, int longitud, int ancho);
+
+
+		int snapCounter;
+		ofImage imagen;
+		
+
+
+
+		bool play;
+		float coorX[512];
+		float coorY[512];
+		float iniY[512];
+		float value[512];
+
+
+
+
+		ofxOscReceiver	receiver;
+		std::string msg;
+		std::string addr;
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		void keyPressed(int key);
 		void keyReleased(int key);
